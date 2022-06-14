e4490667313225011e484277712d8931938a969a
hunghpzo9 <hung.nt20@gmail.com>
2022-06-15 00:15:08 +0700

    appointment list and details

---
+++ src/main/java/com/example/telecare/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.telecare.exception;
+
+public class NotFoundException extends RuntimeException{
+    private  static final long  serialVersionUID =1L;
+
+    public NotFoundException(String msg){
+        super(msg);
+    }
+}
