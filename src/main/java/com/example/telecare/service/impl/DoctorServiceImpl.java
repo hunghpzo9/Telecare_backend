20e041370323e8b428bc4f3f2f90bdba937755f7
ducdqhe141450 <ducdqhe141450@fpt.edu.vn>
2022-07-04 22:08:39 +0700

    them certi doctor

--- src/main/java/com/example/telecare/service/impl/DoctorServiceImpl.java
+++ src/main/java/com/example/telecare/service/impl/DoctorServiceImpl.java
@@ -107,6 +107,26 @@ public class DoctorServiceImpl implements DoctorService {
                 return doctorDTOInf.getIsActive();
             }
 
+            @Override
+            public String getCertificate() {
+                return doctorDTOInf.getCertificate();
+            }
+
+            @Override
+            public String getIdentificationFront() {
+                return doctorDTOInf.getIdentificationFront();
+            }
+
+            @Override
+            public String getIdentificationBack() {
+                return doctorDTOInf.getIdentificationBack();
+            }
+
+            @Override
+            public Date getExpireDateCertificate() {
+                return doctorDTOInf.getExpireDateCertificate();
+            }
+
 
             @Override
             public Integer getAppointmentDoneCount() {
