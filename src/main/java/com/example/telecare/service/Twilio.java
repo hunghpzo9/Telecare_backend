ddc0d503a5eb43aeb1ef7ba471cf1cc030a1172c
hunghpzo9 <hung.nt20@gmail.com>
2022-07-07 00:11:57 +0700

    dit me intellij

--- src/main/java/com/example/telecare/service/Twilio.java
+++ src/main/java/com/example/telecare/service/Twilio.java
@@ -3,7 +3,7 @@ package com.example.telecare.service;
 import com.example.telecare.dto.TwilioRequestDTO;
 import org.springframework.http.ResponseEntity;
 
-public interface TwilioService {
+public interface Twilio {
 
     ResponseEntity sendOtpForPasswordReset(TwilioRequestDTO twilioRequestOTP);
 
