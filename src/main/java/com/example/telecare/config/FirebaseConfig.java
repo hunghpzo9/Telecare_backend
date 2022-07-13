b7816aa22d232ddbfc31711921aba717865e51df
hunghpzo9 <hung.nt20@gmail.com>
2022-07-14 00:41:42 +0700

    test firebase file

--- src/main/java/com/example/telecare/config/FirebaseConfig.java
+++ src/main/java/com/example/telecare/config/FirebaseConfig.java
@@ -8,13 +8,16 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Objects;
 
 @Configuration
 public class FirebaseConfig {
     public void initialization(){
         FileInputStream serviceAccount;
         try{
-            File file = ResourceUtils.getFile("serviceAccountKey.json");
+            ClassLoader classLoader = FirebaseConfig.class.getClassLoader();
+            File file1=new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+            File file = ResourceUtils.getFile("classpath:serviceAccountKey.json");
             serviceAccount = new FileInputStream(file.getAbsolutePath());
 
             FirebaseOptions options = new FirebaseOptions.Builder()
