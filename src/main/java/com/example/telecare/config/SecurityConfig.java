83c0141a947c3ec77319039a1f0340bd4226b672
hunghpzo9 <hung.nt20@gmail.com>
2022-07-17 22:26:43 +0700

    return ipn permit all

--- src/main/java/com/example/telecare/config/SecurityConfig.java
+++ src/main/java/com/example/telecare/config/SecurityConfig.java
@@ -75,9 +75,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/relative/**").hasAnyAuthority(Constants.ROLE_PATIENT, Constants.ROLE_ADMIN)
                 .antMatchers("/api/v1/address/**").hasAnyAuthority(Constants.ROLE_PATIENT,
                         Constants.ROLE_ADMIN, Constants.ROLE_DOCTOR)
+                .antMatchers(HttpMethod.GET,"/api/v1/payment/returnIpn").permitAll()
                 .antMatchers("/api/v1/payment/**").hasAnyAuthority(Constants.ROLE_PATIENT,
                         Constants.ROLE_ADMIN, Constants.ROLE_DOCTOR)
-                .antMatchers("/api/v1/payment/returnIpn").permitAll()
                 .antMatchers("/api/v1/notification/**").hasAnyAuthority(Constants.ROLE_PATIENT,Constants.ROLE_DOCTOR)
 
                 .anyRequest().authenticated()
