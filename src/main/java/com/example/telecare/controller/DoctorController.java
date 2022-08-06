3a2958bf6e535545d56778056d50c272596e7c3c
ducdqhe141450 <ducdqhe141450@fpt.edu.vn>
2022-08-06 20:58:39 +0700

    pull main 858

--- src/main/java/com/example/telecare/controller/DoctorController.java
+++ src/main/java/com/example/telecare/controller/DoctorController.java
@@ -3,11 +3,14 @@ import com.example.telecare.dto.interfaces.DoctorDTOInf;
 import com.example.telecare.dto.DoctorUpdateDTO;
 import com.example.telecare.dto.ResponseOkMessage;
 import com.example.telecare.service.impl.DoctorServiceImpl;
+import com.example.telecare.service.impl.NotificationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -17,7 +20,8 @@ import java.util.List;
 public class DoctorController {
     @Autowired
     DoctorServiceImpl doctorService;
-
+    @Autowired
+    NotificationServiceImpl notificationService;
 
     @GetMapping(value = "/{id}")
     public DoctorDTOInf findDoctorDetail(@PathVariable int id) {
@@ -75,6 +79,12 @@ public class DoctorController {
     public ResponseEntity<Integer> getNumberOfDoctor(@RequestParam String searchText) {
         int medicines = doctorService.getNumberOfDoctor(searchText);
         return new ResponseEntity<Integer>(medicines, HttpStatus.OK);
+
+    }
+    @PostMapping(value = "/sendNotificationFromAdmin")
+    public ResponseEntity<?> sendNotificationFromAdmin(@RequestParam int id, @RequestParam String content) {
+        notificationService.sendNotification(id,"Thông báo từ admin: "+content);
+        return ResponseEntity.ok(new ResponseOkMessage("Gửi thông báo thành công", new Date()));
 
     }
 
