8888fb19c73854695c9ba53d4a58dd8479c9bad2
hoangck2000 <nguyenhoangck2000@gmail.com>
2022-08-09 22:41:24 +0700

    nothing change

--- src/main/java/com/example/telecare/controller/PatientController.java
+++ src/main/java/com/example/telecare/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.example.telecare.controller;
 import com.example.telecare.dto.PatientDTO;
 import com.example.telecare.dto.interfaces.PatientDTOInf;
 import com.example.telecare.dto.interfaces.PatientDTOAdminInf;
+import com.example.telecare.exception.NotFoundException;
 import com.example.telecare.service.impl.AddressServiceImpl;
 import com.example.telecare.service.impl.PatientServiceImpl;
 import com.example.telecare.service.impl.UserServiceImpl;
@@ -27,21 +28,29 @@ public class PatientController {
 
     @GetMapping(value = "/{id}")
     public PatientDTOInf findPatient(@PathVariable int id) {
-      return  patientService.findPatientById(id);
+        if (id < 1) {
+            throw new NotFoundException("Patient Not found");
+        } else {
+            return patientService.findPatientById(id);
+        }
     }
+
     @PutMapping(value = "/{id}")
     public ResponseEntity<PatientDTO> updatePatient(@PathVariable("id") int id, @RequestBody PatientDTO patientDetail) {
-        patientService.updatePatient(patientDetail,id);
+        patientService.updatePatient(patientDetail, id);
         return ResponseEntity.ok(patientDetail);
     }
+
     @GetMapping(value = "")
     public PatientDTOAdminInf findPatientForAdmin(@RequestParam int id) {
-        return  patientService.findPatientByIdForAdmin(id);
+        return patientService.findPatientByIdForAdmin(id);
     }
+
     @GetMapping(value = "/getAll")
     public ResponseEntity<List<PatientDTOAdminInf>> getAllDoctor(@RequestParam int index, @RequestParam String searchText) {
-        return new ResponseEntity<>(patientService.getAllPatient(index,searchText), HttpStatus.OK);
+        return new ResponseEntity<>(patientService.getAllPatient(index, searchText), HttpStatus.OK);
     }
+
     @GetMapping("/numberOfPatient")
     public ResponseEntity<Integer> getNumberOfDoctor(@RequestParam String searchText) {
         int medicines = patientService.getNumberOfPatient(searchText);
